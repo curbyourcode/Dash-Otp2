231d86cfe422c4285083b5c79bcc9affca255244
Rouzbeh Majlessi <rouz@Rouzs-MacBook-Pro.local>
2019-10-07 12:42:11 -0700

    add pages dashboard buggy need to tree shake

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         FooterComponent,
         NavbarComponent,
-        SidebarComponent
+        SidebarComponent,
+        DashboardComponent
     ],
     imports: [
         BrowserModule,
